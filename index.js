bd44757e4e4678e4a6534a3b13c81f98f3dcd5c4
brunoduan <bruno.duan@gmail.com>
2017-09-05 16:40:06 +0800

    Add support to resolve jsc-formated stack

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const sourceMap = require('source-map')
   , loader = require('path-loader')
   , parse = require('./lib/stack-frame-resolver').resolveStack
 
-const regexV8JSFM = /weex/
-  , regexV8Bundle = /<anonymous>/
+const regexV8JSFMFrame = /weex/
+  , regexJSCJSFMFrame = /native-bundle-main/
+  , regexV8BundleFrame = /<anonymous>/
+  , regexJSCBundleFrame = /\w+:\d+:\d+/
 
-const exception = 'h is not definedundefinedReferenceError: h is not defined\n at eval (eval at _ ((weex):4:20831), <anonymous>:10:226712)\n at ws.consume ((weex):2:23939)\n at As.callback ((weex):2:28618)\n at f ((weex):4:18640)\n at C.callback ((weex):4:21679)\n at (weex):4:18890\n at Array.forEach (native)\n at Object.d [as receiveTasks] ((weex):4:18787)\n at Object.V.$s.(anonymous function) [as callJS] ((weex):1:9421)\n at global.(anonymous function) ((weex):8:12332)';
+const exception = 'h is not definedundefinedReferenceError: h is not defined\n at eval (eval at _ ((weex):4:20831), <anonymous>:10:226355)\n at ws.consume ((weex):2:23939)\n at As.callback ((weex):2:28618)\n at f ((weex):4:18640)\n at C.callback ((weex):4:21679)\n at (weex):4:18890\n at Array.forEach (native)\n at Object.d [as receiveTasks] ((weex):4:18787)\n at Object.V.$s.(anonymous function) [as callJS] ((weex):1:9421)\n at global.(anonymous function) ((weex):8:12332)';
+
+//const exception = '[undefined:9:226355] ReferenceError: Can\'t find variable: h\n \n f@native-bundle-main.js:4:18648\n native-bundle-main.js:4:18895\n forEach@[native code]\n '
 
 /**
  * Correct the line number for weex bundle:
@@ -16,8 +20,16 @@ const exception = 'h is not definedundefinedReferenceError: h is not defined\n a
  *
  * Finally, we need to subtract four from the orignal line number.
  */
-function correctLineNumber(lineNumber) {
+function correctV8LineNumber(lineNumber) {
   return lineNumber -= 4;
+}
+
+function correctJSCLineNumber(lineNumber) {
+  return lineNumber -= 3;
+}
+
+function correctJSCColumnNumber(columnNumber) {
+  return columnNumber--;
 }
 
 function getOriginalPositionFor (smc, line, column) {
@@ -42,19 +54,46 @@ function parseException(jsfmMapConsumer, bundleMapConsumer, ex) {
     lineNumber = parseInt(lineNumber, 10)
     columnNumber = parseInt(columnNumber, 10)
     var pos;
-    // Resolve the frame stack from main.js
-    if (regexV8JSFM.test(fileName)) {
+    var isJSCBundleFrame = false;
+    // Resolve the frame stack, there are four types of frames for now:
+    // a) The stack is thrown by v8 and the frame is at jsfm level 
+    // b) The stack is thrown by jsc and the frame is at jsfm level
+    // c) The stack is thrown by v8 and the frame is at bundle level 
+    // d) The stack is thrown by jsc and the frame is at bundle level
+    if (regexV8JSFMFrame.test(fileName) || regexJSCJSFMFrame.test(fileName)) {
       pos = getOriginalPositionFor(jsfmMapConsumer, lineNumber, columnNumber);
-    } else if (regexV8Bundle.test(fileName)) {
-      lineNumber = correctLineNumber(lineNumber);
+    } else if (regexV8BundleFrame.test(fileName)) {
+      lineNumber = correctV8LineNumber(lineNumber);
       pos = getOriginalPositionFor(bundleMapConsumer, lineNumber, columnNumber);
+    } else if (functionName && regexJSCBundleFrame.test(functionName)) {
+      var items = functionName.match(regexJSCBundleFrame);
+      var item = null;
+
+      isJSCBundleFrame = true;
+      if (items) {
+        item = items[0];
+        functionName = functionName.replace(regexJSCBundleFrame, '')
+        functionName = functionName.replace(/\[\]\s*/, '')
+      }
+      items = item.match(/\d+/g);
+      if (items.length == 2) {
+        var ln = parseInt(items[0]);
+        var cn = parseInt(items[1]);
+        lineNumber = correctJSCLineNumber(ln);
+        columnNumber = correctJSCColumnNumber(cn);
+        pos = getOriginalPositionFor(bundleMapConsumer, lineNumber, columnNumber);
+      }
     } else {
       pos = null;
     }
 
     if (pos) {
       const name = pos.name || '';
-      stack.push([name, [pos.source, pos.line, pos.column].join(':')].join(' '));
+      var frame = [name, [pos.source, pos.line, pos.column].join(':')].join(' ');
+      if (isJSCBundleFrame) {
+        frame = [functionName, frame].join(' ');
+      }
+      stack.push(frame);
     } else {
       if (!lineNumber) {
         stack.push([functionName, fileName].join(' '))
